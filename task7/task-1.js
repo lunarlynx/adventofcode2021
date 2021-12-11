8109c5e4813519bdc969a78feeec7e6d6550b7f6
Sofia Kozhevnikova <Kozhevnikova.SS@esphere.ru>
2021-12-11 17:18:48 +0300

    day 7

---
+++ task7/task-1.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const fs = require('fs');
+
+const buffer = fs.readFileSync('input.txt');
+
+const crabArray = String(buffer).trim().split(",").map(el => Number(el));
+
+// Найти максимум из расстояний - это будет число точек, от которых будем считать топливо
+const maxCrab = Math.max.apply(null, crabArray);
+
+// Для каждой точки посчитать расстояние до нее от каждого краба
+// Суммировать все затраты
+// Взять наименьшую сумму
+function fuelsForOnePoint(crabArray) {
+    let minSum = undefined;
+    for (let i = 0; i <= maxCrab; i++) {
+        let s = 0;
+        for (let j = 0; j < crabArray.length; j++) {
+            s = s + Math.abs(crabArray[j] - i);
+        }
+        if (minSum === undefined || s <= minSum) {
+            minSum = s;
+        }
+    }
+    return minSum;
+}
+
+console.log(fuelsForOnePoint(crabArray));
---
+++ task7/task-2.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs = require('fs');
+
+const buffer = fs.readFileSync('input.txt');
+
+const crabArray = String(buffer).trim().split(",").map(el => Number(el));
+
+// Найти максимум из расстояний - это будет число точек, от которых будем считать топливо
+const maxCrab = Math.max.apply(null, crabArray);
+
+// Для каждой точки посчитать расстояние до нее от каждого краба
+// Усложняется тем, что считать мы должны n + (n-1) + (n-2) + ... + (n - n)
+// Суммировать все затраты
+// Взять наименьшую сумму
+function fuelsForOnePoint(crabArray) {
+    let minSum = undefined;
+    for (let i = 0; i <= maxCrab; i++) {
+        let s = 0;
+        for (let j = 0; j < crabArray.length; j++) {
+            let justS = Math.abs(crabArray[j] - i);
+            let sumJustS = 0;
+            for (let k = 0; k <= justS; k++) {
+                sumJustS = sumJustS + k;
+            }
+            s = s + sumJustS;
+        }
+        if (minSum === undefined || s <= minSum) {
+            minSum = s;
+        }
+    }
+    return minSum;
+}
+
+console.log(fuelsForOnePoint(crabArray));
