1bfdfbbaf8cdde8f1de247e45661c774d5f5edef
Sofia Kozhevnikova <Kozhevnikova.SS@esphere.ru>
2021-12-22 16:17:14 +0300

    getValue function for i-1 && j-1 value

--- task9/task-1.js
+++ task9/task-1.js
@@ -6,6 +6,12 @@ const buffer = fs.readFileSync('input.txt');
 
 const data = String(buffer).trim().split("\n").map(el => el.split("").map(Number));
 
+function getValue(arr, i, j) {
+    let arrElement = arr[i] ?? [];
+
+    return arrElement[j];
+}
+
 // Ищем все точки, вокруг которых находятся числа крупнее этой точки
 function findLowPoints(array) {
     let count = 0;
@@ -14,10 +20,10 @@ function findLowPoints(array) {
 
     for (let i = 0; i < max; i++) {
         for (let j = 0; j < array[i].length; j++) {
-            let minus_i = array[i - 1][j] ?? undefined;
-            let minus_j = array[i][j - 1] ?? undefined;
-            let plus_i = array[i + 1][j] ?? undefined;
-            let plus_j = array[i][j + 1] ?? undefined;
+            let minus_i = getValue(array, i - 1, j);
+            let minus_j = getValue(array, i, j - 1);
+            let plus_i = getValue(array, i + 1, j);
+            let plus_j = getValue(array, i, j + 1);
 
             if ((minus_j === undefined || array[i][j] < array[i][j - 1]) &&
                 (plus_i === undefined || array[i][j] < array[i + 1][j]) &&
@@ -30,7 +36,7 @@ function findLowPoints(array) {
             }
         }
     }
-    return count;
+    return count + sum;
 }
 
 console.log(findLowPoints(data));
