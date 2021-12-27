8a7eb4525e233c2829ef2ae8261885d062d11720
Sofia Kozhevnikova <Kozhevnikova.SS@esphere.ru>
2021-12-27 12:21:58 +0300

    day 11, part 2

---
+++ task11/task-2.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const fs = require('fs');
+
+const buffer = fs.readFileSync('input.txt');
+
+const data = String(buffer).trim().split("\n").map(el => String(el).split(""));
+const octopuses = data.map(el => el.map(e => Number(e)));
+
+function getValue(arr, i, j) {
+    let arrElement = arr[i] ?? [];
+
+    return arrElement[j];
+}
+
+// эффект вспышки, добавляем всем окружающим клеткам единицу
+function flashEffect(array, i, j) {
+
+    array[i][j] = 0;
+
+    let lefttop = getValue(array, i - 1, j - 1);
+    let centertop = getValue(array, i, j - 1);
+    let righttop = getValue(array, i + 1, j - 1);
+    let centerleft = getValue(array, i - 1, j);
+    let rightcenter = getValue(array, i + 1, j);
+    let leftbottom = getValue(array, i - 1, j + 1);
+    let centerbottom = getValue(array, i, j + 1);
+    let rightbottom = getValue(array, i + 1, j + 1);
+
+    if (lefttop !== undefined && lefttop > 0) {
+        array[i - 1][j - 1] = lefttop + 1;
+    }
+    if (centertop !== undefined && centertop > 0) {
+        array[i][j - 1] = centertop + 1;
+    }
+    if (righttop !== undefined && righttop > 0) {
+        array[i + 1][j - 1] = righttop + 1;
+    }
+    if (centerleft !== undefined && centerleft > 0) {
+        array[i - 1][j] = centerleft + 1;
+    }
+    if (rightcenter !== undefined && rightcenter > 0) {
+        array[i + 1][j] = rightcenter + 1;
+    }
+    if (leftbottom !== undefined && leftbottom > 0) {
+        array[i - 1][j + 1] = leftbottom + 1;
+    }
+    if (centerbottom !== undefined && centerbottom > 0) {
+        array[i][j + 1] = centerbottom + 1;
+    }
+    if (rightbottom !== undefined && rightbottom > 0) {
+        array[i + 1][j + 1] = rightbottom + 1;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] > 9) {
+                array = flashEffect(array, i, j);
+            }
+        }
+    }
+
+    return array;
+}
+
+// добавляем энергию каждому осьминогу, ждем вспышку и добавляем энергию всем осьминогам вокруг
+function countFlash(array) {
+    let count = 0;
+    let step = 0;
+    let maxCount = array[0].length * array.length;
+
+
+    while (count !== maxCount) {
+
+        step++;
+
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                array[i][j] = array[i][j] + 1;
+            }
+        }
+
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                if (array[i][j] > 9) {
+                    array = flashEffect(array, i, j);
+                }
+            }
+        }
+
+        count = 0;
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                if (array[i][j] === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return step;
+}
+
+
+console.log(countFlash(octopuses));
